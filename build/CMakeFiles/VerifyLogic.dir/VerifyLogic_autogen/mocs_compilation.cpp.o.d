a047d456cf949a369d36b99f9766eca83055b13d
Geovanni Gonzalez <geovanniga32@gmail.com>
2026-07-06 11:42:54 -0600

    Remove CMake build artifacts

--- build/CMakeFiles/VerifyLogic.dir/VerifyLogic_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/VerifyLogic.dir/VerifyLogic_autogen/mocs_compilation.cpp.o: \
- /mnt/c/Users/geova/Github\ Desktop\ Repos/VisualizadorDeGrafos-GUI-App/build/VerifyLogic_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/VisualizadorDeGrafos.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for VisualizadorDeGrafos.
-# This may be replaced when dependencies are built.
